7552e2e2e194bd900a52bf68b4b9148ce5963796
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2026-04-28 11:17:30 +0300

    Include lazy artifacts when bundling

--- src/bundling.jl
+++ src/bundling.jl
@@ -19,7 +19,7 @@ function bundle_products(recipe::BundleRecipe)
     stdlibs = unique(vcat(PackageCompiler.gather_stdlibs_project(ctx2),
                           intersect(PackageCompiler._STDLIBS, map(x->x.name, Base._sysimage_modules))))
     PackageCompiler.bundle_julia_libraries(recipe.output_dir, stdlibs)
-    PackageCompiler.bundle_artifacts(ctx2, recipe.output_dir; include_lazy_artifacts=false) # Lazy artifacts
+    PackageCompiler.bundle_artifacts(ctx2, recipe.output_dir; include_lazy_artifacts=true) # Lazy artifacts
     PackageCompiler.bundle_cert(recipe.output_dir) # SSL certificates
 
     # Re-home bundled libraries into the desired bundle layout
